c00f9974aeca19a2b214b59a8596506fcf771afb
Devin Torres <dvntor@meta.com>
2024-07-26 17:17:44 -0700

    routing for search bar result page

--- app/src/components/events/FuzzyishSearchBar.tsx
+++ app/src/components/events/FuzzyishSearchBar.tsx
@@ -5,15 +5,13 @@ import { Search } from "lucide-react";
 import { Input } from "../ui/input";
 import { FormEvent, useEffect, useState } from "react";
 import { collection, getDocs, where, query, Query, DocumentData, QueryFieldFilterConstraint, or } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 export function FuzzyishSearchBar() {
     const [userInput, setUserInput] = useState<string>();
     const [queryToPerform, setQueryToPerform] = useState<Query<DocumentData>>();
 
-    async function fetchQuery(queryToPerform: Query<DocumentData>) {
-        const querySnap = await getDocs(queryToPerform);
-        window.localStorage.setItem("Events Queried", JSON.stringify(querySnap.docs))
-    }
+    const navigate = useNavigate();
 
 
     function constructQuery(searchTerms: any) {
@@ -110,6 +108,11 @@ export function FuzzyishSearchBar() {
         
     }
 
+    async function fetchQuery(queryToPerform: Query<DocumentData>) {
+        const querySnap = await getDocs(queryToPerform);
+        window.localStorage.setItem("Events Queried", JSON.stringify(querySnap.docs))
+    }
+
     function handleEventSearch(e: FormEvent<Element>): void {
         e.preventDefault();
         window.localStorage.removeItem("Events Queried");
@@ -118,6 +121,7 @@ export function FuzzyishSearchBar() {
         if (queryToPerform) {
             fetchQuery(queryToPerform)
         }
+        navigate(`/events/search/${userInput}`)
         
     }
 
